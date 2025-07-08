fdf29343734ad4de4d71fd577080bf4e94cd50e4
sanju-010 <sanjuworkmail459@gmail.com>
2025-07-08 16:23:44 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
 const CACHE_NAME = 'expense-tracker-v1';
 const urlsToCache = [
-  '/Expense-Tracker/',                // <-- this must match
+  '/Expense-Tracker/',
   '/Expense-Tracker/index.html',
   '/Expense-Tracker/style.css',
   '/Expense-Tracker/app.js',
+  '/Expense-Tracker/chart.min.js',
   '/Expense-Tracker/manifest.json',
   '/Expense-Tracker/icon-192.png',
   '/Expense-Tracker/icon-512.png'
@@ -11,15 +12,12 @@ const urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
